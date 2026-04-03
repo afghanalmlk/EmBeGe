fbf1ac98821a445f9e39581542a434be816e9c55
afghanalmlk <afghanalmalik2404@gmail.com>
2026-04-03 18:13:20 +0700

    Update sppgRoutes.js

--- backend/src/routes/sppgRoutes.js
+++ backend/src/routes/sppgRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
 const { getSppg } = require('../controllers/sppgController');
+const { verifyToken } = require('../middlewares/authMiddleware');
 
-router.get('/', getSppg);
-
+router.get('/', verifyToken, getSppg);
 module.exports = router;
